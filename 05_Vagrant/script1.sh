82509b2a8ac466a0e9f5e2f3d3e161d3183fa4e9
Elton Torres <elton.bt@gmail.com>
2026-09-15 18:25:10 -0300

    refactor: update Vagrantfiles and playbooks for consistent box usage and improved Nginx configuration

--- 05_Vagrant/script1.sh
+++ 05_Vagrant/script1.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-sudo cat /vagrant/ansible.cfg > /etc/ansible/ansible.cfg
-sudo cat /vagrant/hosts > /etc/ansible/hosts
+sudo mkdir -p /etc/ansible
+sudo cp -f /vagrant/ansible.cfg /etc/ansible/ansible.cfg
+sudo cp -f /vagrant/hosts /etc/ansible/hosts
 rm -f /vagrant/key*
 ssh-keygen -q -t rsa -f /vagrant/key -N ''
